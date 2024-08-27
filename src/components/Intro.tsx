26d0b85d0c36b7bfc3df3f1d7cba0b3f10acdb5c
Andrew Li <andrew.li@duke.edu>
2024-08-27 16:36:54 -0400

    about me draft 1

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,15 +1,19 @@
 const Intro: React.FC = () => {
     return (
         <div className="intro-container">
-            <img src="/public/images/head.JPG" className="profile-picture"></img>
+            <div className="intro-photo-container">
+                <img src="/public/images/head.JPG" className="profile-picture" alt="Profile Picture"></img>
+            </div>
             <div className="intro-text"> 
-                <h1> Hi. I'm Andrew Li. </h1>
+                <h1> Andrew Li </h1>
                 <h3>
-                    I’m a sophmore at Duke University majoring in computer science. I want to make peoples’ lives easier and 
-                    more enjoyable using technology.  While I currently have a strong interest in software engineering, I’m 
-                    also open to exploring other areas within the tech field where I can learn and grow. I love personal growth, 
-                    so I’m always looking for opportunities to expand my skills and take on new challenges.
+                    I’m a sophomore at Duke University majoring in computer science. 
+                    I want to make peoples’ lives easier and more enjoyable using technology.  
+                    While I currently have a strong interest in software engineering, I’m 
+                    also open to exploring other areas within the tech field where I can learn and grow. 
+                    I love personal growth, so I’m always looking for opportunities to expand my skills and take on new challenges.
                 </h3>
+                <button className="intro-button"> Learn more about me and my projects! </button>
             </div>
         </div>
     )
