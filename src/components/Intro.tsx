1cf7288a46ae4d53997ece727c634d5f0615f6b9
Andrew Li <andrew.li@duke.edu>
2024-10-16 21:06:37 -0400

    head.JPG

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,7 +2,7 @@ const Intro: React.FC = () => {
     return (
         <div id="About" className="intro-container">
             <div className="intro-photo-container">
-                <img src="/images/head.jpg" className="profile-picture" alt="Profile Picture"></img>
+                <img src="/images/head.JPG" className="profile-picture" alt="Profile Picture"></img>
             </div>
             <div className="intro-text"> 
                 <h1> Andrew Li </h1>
